0729b9bb8a32026f95dd103d3c19a7249dbbc295
Adam Ki Jensen <adajensen@gmail.com>
2015-08-25 14:24:58 -0600

    CRUD completed

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,14 +2,30 @@ require 'models/task_manager'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
+  set :method_override, true
 
   get '/' do
     erb :dashboard
   end
 
+  put '/tasks/:id' do |id|
+    TaskManager.update(id.to_i , params[:task])
+    redirect "tasks/#{id}"
+  end
+
   get '/tasks' do
     @tasks = TaskManager.all  #instance variable w/ 3 strings
     erb :index                            #render index.erb
+  end
+
+  get '/tasks/:id/edit' do |id|
+    @task = TaskManager.find(id.to_i)
+    erb :edit
+  end
+
+  delete "/tasks/:id" do |id|
+    TaskManager.delete(id.to_i)
+    redirect '/tasks'
   end
 
   get '/tasks/new' do
